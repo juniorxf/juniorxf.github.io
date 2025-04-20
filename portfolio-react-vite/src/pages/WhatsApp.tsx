116fd4ffc8549cbca7646b3a29be8f50f678be91
José Freitas <juniorxf@gmail.com>
2025-04-19 21:47:55 -0300

    layout ok

--- portfolio-react-vite/src/pages/WhatsApp.tsx
+++ portfolio-react-vite/src/pages/WhatsApp.tsx
@@ -1,18 +1,18 @@
 // src/pages/WhatsApp.tsx
-import { useState } from 'react';
-import LoginPhone from '../components/WhatsAppPhone/LoginPhone';
+// import { useState } from 'react';
+// import LoginPhone from '../components/WhatsAppPhone/LoginPhone';
 import WhatsAppPhone from '../components/WhatsAppPhone/WhatsAppPhone';
 
-export default function WhatsApp() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  return !isAuthenticated ? (
-    <LoginPhone onSuccess={() => setIsAuthenticated(true)} />
-  ) : (
-    <WhatsAppPhone />
-  );
-}
-
 // export default function WhatsApp() {
-//   return <WhatsAppPhone />
+//   const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+//   return !isAuthenticated ? (
+//     <LoginPhone onSuccess={() => setIsAuthenticated(true)} />
+//   ) : (
+//     <WhatsAppPhone />
+//   );
 // }
+
+export default function WhatsApp() {
+  return <WhatsAppPhone />
+}
